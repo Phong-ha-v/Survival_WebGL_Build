14083fd147f9b8f66d9fa34c576212a6f0d3daf4
Phong Ha <phonghv@onechain.game>
2024-12-09 16:05:36 +0700

    New Build

---
+++ Bridge.js
@@ -0,0 +1,22 @@
+// telegram.js
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.hash.substring(1));
+    return params.get("tgWebAppData");
+};
+function getRefCode() {
+    const urlHash = window.location.hash;
+    const textSearch = window.location.search;
+
+      if (urlHash) {
+        const encodedData = urlHash.slice(1);
+        const data = encodedData.split("&");
+        const tgWebAppData = decodeURIComponent(
+          data[0].split("tgWebAppData=")[1]
+        );
+
+        const refCode = textSearch
+          ? textSearch.split("?tgWebAppStartParam=")[1]
+          : undefined;
+        return refCode;
+      }
+}
